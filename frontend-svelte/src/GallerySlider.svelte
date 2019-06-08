d26edaec4d4cc44d0839edd7c82fa2a2cf018c76
Andre Alves Garzia <andre@andregarzia.com>
2019-06-08 14:11:56 +0100

    layout for location (wip)

---
+++ frontend-svelte/src/GallerySlider.svelte
@@ -0,0 +1,71 @@
+<script>
+  let locations = false;
+
+  async function getLocations() {
+    const res = await fetch(
+      "http://dev.himalayanacademy.com/virtualtour/index.php/locations"
+    );
+
+    const data = await res.json();
+
+    if (res.ok) {
+      locations = data.locations.slice(0, 5);
+    } else {
+      console.error("Error fetching locations", res);
+    }
+  }
+
+  getLocations();
+</script>
+
+<style>
+  .sliders {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100px;
+    max-height: 100px;
+    background-color: #8c3945;
+    flex-wrap: nowrap;
+    overflow-x: hidden;
+  }
+
+  .item {
+    padding: 3px;
+  }
+
+  .arrow {
+      width: 120px;
+  }
+
+  img {
+    border-radius: 20px;
+    border: solid 3px #8c3945;
+    max-height: 80px;
+    overflow: hidden;
+  }
+</style>
+
+<div class="sliders">
+  {#if locations}
+    <a class="item arrow" href="#">
+      <img
+        src="http://dev.himalayanacademy.com/virtualtour/points-of-interest/arrow-l.svg"
+        alt="left" />
+    </a>
+    {#each locations as location}
+      <a class="item" href="#/location/{location.id}">
+        <img
+          src="http://dev.himalayanacademy.com/virtualtour/index.php/thumb/500/{location.id}/{location.hero_image}"
+          alt={location.title} />
+      </a>
+    {/each}
+    <a class="item arrow" href="#">
+      <img
+        src="http://dev.himalayanacademy.com/virtualtour/points-of-interest/arrow-r.svg"
+        alt="right"/>
+    </a>
+  {:else}
+    <p>Loading</p>
+  {/if}
+</div>
--- frontend-svelte/src/Item.svelte
+++ frontend-svelte/src/Item.svelte
@@ -1,29 +1,15 @@
 <script>
   import { parseLocation } from "./routes.js";
+  import GallerySlider from "./GallerySlider.svelte";
 
-  let promise = getLocations();
-
-  let locations = false;
   let location = false;
-
-  async function getLocations() {
-    const res = await fetch(
-      "http://dev.himalayanacademy.com/virtualtour/index.php/locations"
-    );
-
-    const data = await res.json();
-
-    if (res.ok) {
-      locations = data.locations;
-    } else {
-      console.error("Error fetching locations", res);
-    }
-  }
 
   async function getLocation(id) {
     console.log("id", id);
-    const res = await fetch(`http://dev.himalayanacademy.com/virtualtour/index.php/location/${id}`);
-    
+    const res = await fetch(
+      `http://dev.himalayanacademy.com/virtualtour/index.php/location/${id}`
+    );
+
     const data = await res.json();
 
     if (res.ok) {
@@ -36,12 +22,108 @@
   const params = parseLocation();
 
   getLocation(params.location);
-  getLocations();
 
   $: {
     console.log("location", location);
-    console.log("locations", locations);
   }
 </script>
 
-<div />
+<style>
+  .flex {
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    height: calc(100vh - 60px);
+    max-height: calc(100vh - 60px);
+  }
+
+  .main-display {
+    flex: 1 1 auto;
+    align-self: auto;
+    display: flex;
+    height: calc(100vh - 180px);
+    max-height: calc(100vh - 160px);
+  }
+
+  .current-tab {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    vertical-align: top;
+    justify-content: center;
+  }
+
+  .current-tab figure.hero {
+    max-height: 40%;
+    max-width: 60%;
+    text-align: center;
+  }
+
+  figure.hero img {
+    border-radius: 20px;
+    border: 3px solid #8c3945;
+    max-width: 100%;
+    max-height: 100%;
+  }
+
+  .current-tab img.feet {
+    width: 70%;
+    padding: 20px;
+  }
+
+  .current-tab p {
+    font-size: 16px;
+    padding: 10px;
+  }
+
+  .tab-switcher {
+    width: 20%;
+    min-width: 20%;
+    background-color: #b7c1c1;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .arrow-1 {
+    margin-left: -150px;
+  }
+
+  h1 {
+    color: rgb(140, 57, 69);
+    font-family: Lato, sans-serif;
+    font-size: 48px;
+    margin: 0;
+    padding: 0;
+  }
+
+ 
+</style>
+
+<div class="container flex">
+  {#if location}
+    <div class="main-display">
+      <div class="container current-tab">
+        <h1>{location.metadata.title}</h1>
+        <p>{location.metadata.description}</p>
+        <figure class="hero">
+          <img
+            src="http://dev.himalayanacademy.com/virtualtour/{location.metadata.hero_image}"
+            alt={location.metadata.title} />
+        </figure>
+        <img
+          class="feet"
+          src="http://dev.himalayanacademy.com/virtualtour/points-of-interest/feet.svg"
+          alt="decoration" />
+      </div>
+      <div class="tab-switcher">
+        <img
+          class="arrow-1"
+          src="http://dev.himalayanacademy.com/virtualtour/points-of-interest/arrow_1.svg"
+          alt="decoration" />
+      </div>
+    </div>
+    <GallerySlider class="sliders" />
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</div>
